59cfcc249f79e39d154be4d8b71ca5e9de854979
namtran232 <namphongtran232@gmail.com>
2023-01-04 13:04:23 +0700

    Dựng khung Layout mặc định

--- src/component/Layout/DefaultLayout/Sidebar/index.js
+++ src/component/Layout/DefaultLayout/Sidebar/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import styles from './Sidebar.module.scss';
+import classNames from 'classnames/bind';
 
+const cx = classNames.bind(styles)
 function Sidebar() {
   return (
-    <h1>Sidebar</h1>
+    <aside className={cx('wrapper')}>
+      <h2>Sidebar</h2>
+    </aside>
   )
 }
 
