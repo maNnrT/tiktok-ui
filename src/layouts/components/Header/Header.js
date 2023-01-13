320bae97929ba37d8952ff1c3aef7c6b48ec1cac
namtran232 <namphongtran232@gmail.com>
2023-01-14 03:02:21 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/layouts/components/Header/Header.js
@@ -0,0 +1,144 @@
+import { Fragment } from 'react';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleQuestion,
+  faEllipsisVertical,
+  faA,
+  faKeyboard,
+  faUser,
+  faCoins,
+  faTools,
+  faRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+import { Link } from 'react-router-dom';
+import config from '~/config';
+
+import Button from '~/component/Button';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import Menu from '~/component/Popper/Menu';
+import { InboxIcon, MessageIcon, UploadIcon } from '~/component/Icons';
+import Image from '~/component/Image';
+import Search from '../Search';
+const cx = classNames.bind(styles);
+const MENU_ITEMS = [
+  {
+    icon: <FontAwesomeIcon icon={faA} />,
+    title: 'English',
+    children: {
+      title: 'Language',
+      data: [
+        {
+          type: 'language',
+          code: 'en',
+          title: 'English',
+        },
+        {
+          type: 'language',
+          code: 'vi',
+          title: 'Tiếng Việt',
+        },
+      ],
+    },
+  },
+  {
+    icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+    title: 'Feedback and help',
+    to: '/feedback',
+  },
+  {
+    icon: <FontAwesomeIcon icon={faKeyboard} />,
+    title: 'Keyboard shortcuts',
+  },
+];
+function Header() {
+  const handleMenuChange = (menuItem) => {
+    switch (menuItem.type) {
+      case 'language':
+        //handle
+        break;
+      default:
+    }
+  };
+  const currentUser = true;
+  const userMenu = [
+    {
+      icon: <FontAwesomeIcon icon={faUser} />,
+      title: 'View profile',
+      to: '@hoaa',
+    },
+    {
+      icon: <FontAwesomeIcon icon={faCoins} />,
+      title: 'Get coin',
+      to: '/coin',
+    },
+    {
+      icon: <FontAwesomeIcon icon={faTools} />,
+      title: 'Setting',
+      to: '/setting',
+    },
+    ...MENU_ITEMS,
+    {
+      icon: <FontAwesomeIcon icon={faRightFromBracket} />,
+      title: 'Log out',
+      to: '/logout',
+      seperate: true,
+    },
+  ];
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        <Link to={config.routes.home} className={cx('logo-link')}>
+          <img src={images.logo} alt="TikToK" />
+        </Link>
+        <Search />
+
+        <div className={cx('action')}>
+          {currentUser ? (
+            <Fragment>
+              <Tippy delay={[0, 200]} content="Upload" placement="bottom">
+                <button className={cx('action-btn')}>
+                  <UploadIcon />
+                </button>
+              </Tippy>
+              <Tippy delay={[0, 200]} content="Message" placement="bottom">
+                <button className={cx('action-btn')}>
+                  <MessageIcon />
+                </button>
+              </Tippy>
+              <Tippy delay={[0, 200]} content="Inbox" placement="bottom">
+                <button className={cx('action-btn')}>
+                  <InboxIcon />
+                  <span className={cx('badge')}>12</span>
+                </button>
+              </Tippy>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Button text>Upload</Button>
+              <Button primary>Log in</Button>
+            </Fragment>
+          )}
+          <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
+            {currentUser ? (
+              <Image
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/a481917deeab7d399b9ab8f002cb1fa8~c5_100x100.jpeg?x-expires=1673406000&x-signature=TJwV7uOITiUaCee4MfBLicJZbPE%3D"
+                className={cx('user-avatar')}
+                alt="NguyenVanA"
+              />
+            ) : (
+              <button className={cx('more-btn')}>
+                <FontAwesomeIcon icon={faEllipsisVertical} />
+              </button>
+            )}
+          </Menu>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
